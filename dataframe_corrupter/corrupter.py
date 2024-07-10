8d1b10603cc4698bc7773af68e5e5c2250e4478d
Ahmad Muhammad <121247258+ahmadMuhammadGd@users.noreply.github.com>
2024-07-10 17:33:53 +0300

    Update corrupter.py

--- dataframe_corrupter/corrupter.py
+++ dataframe_corrupter/corrupter.py
@@ -17,30 +17,3 @@ class DataFrameCorrupter:
             else:
                 corrupted_df = strategy.corrupt(corrupted_df)
         return corrupted_df.reset_index(drop=True)
-
-'''
-usage examle:
-
-if __name__ == "__main__":
-    # Sample DataFrame
-    data = {
-        'A': [1, 2, 3, 4, 5],
-        'B': [5, 4, 3, 2, 1],
-        'C': [10, 20, 30, 40, 50]
-    }
-    df = pd.DataFrame(data)
-
-    # Initialize the corrupter with no initial strategies
-    corrupter = DataFrameCorrupter()
-
-    # Add strategies with specified columns
-    corrupter.add_strategy(DropRowsStrategy(fraction=0.2), columns=['A', 'B'])
-    corrupter.add_strategy(AddNoiseStrategy(noise_level=0.1), columns=['B', 'C'])
-    corrupter.add_strategy(ShuffleColumnsStrategy())  # This strategy applies to the whole DataFrame
-
-    # Apply the corruptions
-    corrupted_df = corrupter.corrupt(df)
-    print("After Applying Chained Corruptions:")
-    print(corrupted_df)
-    
-'''
