b4dc64a0936fd0cc73360199206bea078cd36578
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-07-10 16:12:10 +0300

    full project

--- dataframe_corrupter/strategy/__init__.py
+++
@@ -1,6 +0,0 @@
-# from .add_nulls_strategy import AddNullsStrategy
-# from .add_typos_strategy import AddTyposStrategy
-# from .add_duplicate_rows_strategy import AddDuplicateRowsStrategy
-# from .add_negative_values_strategy import AddNegativeValuesStrategy
-# from .add_noise_strategy import AddNoiseStrategy
-# from .drop_rows_strategy import DropRowsStrategy
